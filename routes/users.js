e9b36e0dc87b94b41b0703e46269b1f2deb9348f
htetwaiwai <htetwaiwai1999@gmail.com>
2019-11-25 09:47:39 +0630

    add user

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var Eat=require('../model/eatAdrink');
+var See = require('../model/seeAdo');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/home', function(req, res, next) {
+  See.find(function (err,rtn) {
+    if(err) throw err;
+    res.render('users/home',{see:rtn})
+  })
+
 });
 
 module.exports = router;
