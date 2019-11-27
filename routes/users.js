6acf98db1c1f20717134bcfe5dc7e90255193c83
mhwaymhway <mhwayei917@gmail.com>
2019-11-27 14:26:20 +0630

    select from database

--- routes/users.js
+++ routes/users.js
@@ -7,16 +7,57 @@ var Traveller=require('../model/travelleressential');
 var Tour=require('../model/tour');
 /* GET users listing. */
 router.get('/home', function(req, res, next) {
-  See.find(function (err,rtn) {
+  var query={categories:'Arts'};
+  See.find(query,function (err,rtn) {
     if(err) throw err;
-    res.render('users/home',{see:rtn})
+    res.render('users/art',{see:rtn})
   })
 
 });
-router.get('/festivalhome', function(req, res, next) {
-Festival.find(function (err,rtn) {
+router.get('/history', function(req, res, next) {
+  var query={categories:'History'};
+  See.find(query,function (err,rtn) {
     if(err) throw err;
-    res.render('users/festivalhome',{festival:rtn})
+    res.render('users/history',{see:rtn})
+  })
+
+});
+router.get('/culture', function(req, res, next) {
+  var query={categories:'Culture'};
+  See.find(query,function (err,rtn) {
+    if(err) throw err;
+    res.render('users/culture',{see:rtn})
+  })
+
+});
+router.get('/archi', function(req, res, next) {
+  var query={categories:'Architecture'};
+  See.find(query,function (err,rtn) {
+    if(err) throw err;
+    res.render('users/archi',{see:rtn})
+  })
+
+});
+router.get('/nature', function(req, res, next) {
+  var query={categories:'NatureAndWildlife'};
+  See.find(query,function (err,rtn) {
+    if(err) throw err;
+    res.render('users/nature',{see:rtn})
+  })
+});
+router.get('/festival', function(req, res, next) {
+  var query={categories:'Festival'};
+Festival.find(query,function (err,rtn) {
+    if(err) throw err;
+    res.render('users/festival',{festival:rtn})
+  })
+
+});
+router.get('/event', function(req, res, next) {
+  var query={categories:'Event'};
+Festival.find(query,function (err,rtn) {
+    if(err) throw err;
+    res.render('users/event',{festival:rtn})
   })
 
 });
@@ -27,18 +68,34 @@ Traveller.find(function (err,rtn) {
   })
 
 });
-router.get('/tour',function (req ,res, next) {
-  Tour.find(function(err,rtn){
+router.get('/popular',function (req ,res, next) {
+  var query={categories:'Popular Place'}
+  Tour.find(query,function(err,rtn){
     if(err) throw err;
-    res.render('users/tour',{tour:rtn})
+    res.render('users/popular',{tour:rtn})
   })
 });
-
+router.get('/worldhs',function (req ,res, next) {
+  var query={categories:'World Heritage Sites'}
+  Tour.find(query,function(err,rtn){
+    if(err) throw err;
+    res.render('users/worldhs',{tour:rtn})
+  })
+});
 router.get('/eathome',function (req ,res, next) {
   var query={categories:'Eat'};
   Eat.find(query,function(err,rtn){
     if(err) throw err;
     res.render('users/eathome',{eat:rtn})
+
+});
+
+});
+router.get('/drink',function (req ,res, next) {
+  var query={categories:'Drink'};
+  Eat.find(query,function(err,rtn){
+    if(err) throw err;
+    res.render('users/drink',{eat:rtn})
 
 });
 
